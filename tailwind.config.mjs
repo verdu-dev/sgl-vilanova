2a3cbbeed7696493ab31a1d0746060c6c79e5eb5
verdulife <verdu@live.com>
2025-01-19 22:13:28 +0100

    add all sections base

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,16 @@
+import { fontFamily } from 'tailwindcss/defaultTheme.js'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: [
+				"Space Grotesk Variable",
+				fontFamily.sans,
+			],
+		},
+
 		extend: {
 			colors: {
 				sgl: '#ffa348'
