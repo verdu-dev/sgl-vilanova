a44369af883bf2628dfc8777472500a2de7acfc2
verdulife <verdu@live.com>
2025-01-21 21:33:59 +0100

    done til contacto

---
+++ src/lib/images.ts
@@ -0,0 +1,77 @@
+export const totemImages = [
+  {
+    src: "/totem-de-aluminio-1.webp",
+    alt: "Totem de aluminio con base de hormigón"
+  },
+  {
+    src: "/totem-de-aluminio-2.webp",
+    alt: "Totem de aluminio con base de hormigón"
+  },
+  {
+    src: "/totem-de-aluminio-3.webp",
+    alt: "Totem de aluminio con base de hormigón"
+  },
+  {
+    src: "/totem-de-aluminio-4.webp",
+    alt: "Totem de aluminio con base de hormigón"
+  }
+]
+
+export const clientLogos = [
+  {
+    src: "/logos/ajuntament-sitges.svg",
+    alt: "Ajuntament de Sitges"
+  },
+  {
+    src: "/logos/coca-cola.svg",
+    alt: "Coca-Cola"
+  },
+  {
+    src: "/logos/ajuntament-vilanova.svg",
+    alt: "Ajuntament de Vilanova i la Geltrú"
+  },
+  {
+    src: "/logos/primavera-sound.svg",
+    alt: "Primavera Sound"
+  },
+  {
+    src: "/logos/ajuntament-canyelles.svg",
+    alt: "Ajuntament de Canyelles"
+  },
+  {
+    src: "/logos/ajuntament-barcelona.svg",
+    alt: "Ajuntament de Barcelona"
+  },
+  {
+    src: "/logos/mediapro.svg",
+    alt: "Mediapro"
+  },
+  {
+    src: "/logos/aerostar.svg",
+    alt: "Aerostar"
+  },
+  {
+    src: "/logos/sitges-film-festival.svg",
+    alt: "Sitges Film Festival"
+  },
+  {
+    src: "/logos/barcelona-rock-fest.svg",
+    alt: "Barcelona Rock Fest"
+  },
+  {
+    src: "/logos/ajuntament-olivella.svg",
+    alt: "Ajuntament de Olivella"
+  },
+  {
+    src: "/logos/ajuntament-hospitalet.svg",
+    alt: "Ajuntament de L'Hospitalet"
+  },
+  {
+    src: "/logos/ajuntament-santa-coloma.svg",
+    alt: "Ajuntament de Santa Coloma de Gramenet"
+  },
+  {
+    src: "/logos/ajuntament-sant-feliu.svg",
+    alt: "Ajuntament de Sant Feliu de Guíxols"
+  }
+];
--- src/metadata.ts
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "SGL Vilanova | Infraestructuras para eventos",
-  description: "Infraestructuras técnicas certificadas para eventos",
-  image: "card.jpg",
-  name: "SGL Vilanova",
-  url: "https://www.sgl-vilanova.com",
-}
