8f700b341227e0a5c7d1a4278ffe44005941fc65
verdu-dev <verdukactus@gmail.com>
2025-01-25 14:21:35 +0100

    migrate to tailwindcss 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: "https://www.sgl-vilanova.com",
@@ -8,5 +8,7 @@ export default defineConfig({
     locales: ["es", "ca"],
     defaultLocale: "es"
   },
-  integrations: [tailwind()]
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
