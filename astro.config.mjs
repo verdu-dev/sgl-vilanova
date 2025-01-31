b2cb6c6b3df71d5d1553261cd5d151af99d1097b
verdu-dev <verdukactus@gmail.com>
2025-01-31 10:47:13 +0100

    add sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://www.sgl-vilanova.com",
+  site: "https://www.sglvilanova.com",
+
   i18n: {
     locales: ["es", "ca"],
     defaultLocale: "es"
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
